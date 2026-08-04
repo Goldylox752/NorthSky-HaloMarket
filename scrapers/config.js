dfa80a79bf89033ade189184b3b19ef518b98d86
Byron Sanche <261788158+Goldylox752@users.noreply.github.com>
2026-08-04 16:42:51 -0600

    Create config.js

---
+++ scrapers/config.js
@@ -0,0 +1,23 @@
+export const scraperConfig = {
+
+  ebay:{
+    baseUrl:"https://www.ebay.com"
+  },
+
+
+  amazon:{
+    baseUrl:"https://www.amazon.com"
+  },
+
+
+  categories:[
+
+    "electronics",
+    "tools",
+    "gaming",
+    "collectibles",
+    "home"
+
+  ]
+
+};
